86db596830e1dd112446a93e202119687b0bb9d5
Sevelantis <hamer223@o2.pl>
2023-03-09 18:09:01 +0000

    dependency injection

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,2 +1,40 @@
 '''router.py is a core of each module with all the endpoints'''
 
+from http import HTTPStatus
+from urllib.parse import quote_plus, urlencode
+from fastapi import APIRouter, Depends, Request
+from starlette.responses import RedirectResponse
+from src.auth.service import Auth0
+from authlib.integrations.starlette_client import StarletteOAuth2App
+
+import config
+
+auth_router = APIRouter()
+
+
+@auth_router.get('/auth')
+async def auth(request: Request, auth_client : StarletteOAuth2App = Depends(Auth0.client)):
+    token = await auth_client.authorize_access_token(request)
+    request.session['token'] = token
+    return RedirectResponse(url='/', status_code=HTTPStatus.FOUND)
+
+
+@auth_router.get('/login')
+async def login(request: Request, auth_client : StarletteOAuth2App = Depends(Auth0.client)):
+    redirect_uri = request.url_for('auth')
+    return await auth_client.authorize_redirect(request, redirect_uri) # redirects to auth client login page
+
+
+@auth_router.get("/logout")
+async def logout(request: Request):
+    request.session.clear()
+    return RedirectResponse(
+        url=f"https://{config.AUTH0_DOMAIN}/v2/logout?" 
+        + urlencode(
+            {
+                "returnTo": request.url_for('homepage'),
+                "client_id": config.AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        )
+    )
--- src/main.py
+++ src/main.py
@@ -3,18 +3,30 @@
 from http import HTTPStatus
 import json
 from urllib.parse import quote_plus, urlencode
-from fastapi import APIRouter, Depends, FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Depends
 from fastapi.responses import JSONResponse
 import uvicorn
 from starlette.middleware.sessions import SessionMiddleware
-from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import RedirectResponse, HTMLResponse
-from authlib.integrations.starlette_client import OAuth
+from starlette.responses import RedirectResponse
+from src.auth.router import auth_router
 
 import config
 
-app = FastAPI()
+app = FastAPI(
+    title="Converter Web App",
+    description='Converter Web App',
+    version="v1.0.0",
+    terms_of_service="https://en.wikipedia.org/wiki/Terms_of_service",
+    contact={
+        "name": "Our GitHub organization",
+        "url": "https://github.com/EGS-project"
+    },
+    license_info={
+        "name": "Beerware License",
+        "url": "https://en.wikipedia.org/wiki/Beerware",
+    },)
+
 app.add_middleware(
     SessionMiddleware, 
     secret_key=config.APP_SECRET_KEY)
@@ -25,61 +37,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     )
-    
-oauth = OAuth()
-oauth.register(
-    name='auth0',
-    client_id=config.AUTH0_CLIENT_ID,
-    client_secret=config.AUTH0_CLIENT_SECRET,
-    api_base_url=config.AUTH0_DOMAIN,
-    access_token_url=f'https://{config.AUTH0_DOMAIN}/oauth/token',
-    authorize_url=f'https://{config.AUTH0_DOMAIN}/authorize',
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
-    server_metadata_url=f'https://{config.AUTH0_DOMAIN}/.well-known/openid-configuration',
-)
 
-
-# Controllers API
-@app.route('/')
-async def homepage(request):
+@app.get('/')
+async def homepage(request: Request):
     token = request.session.get('token')
     if token:
-        user = request.session.get('user')
         message = {'message': f'Hello! Your token is: {json.dumps(token)}'}
         return JSONResponse(content=message, status_code=HTTPStatus.OK)
     else:
-        return RedirectResponse(url='/login', status_code=HTTPStatus.TEMPORARY_REDIRECT)
-
-
-@app.route('/auth', methods=["GET", "POST"])
-async def auth(request):
-    token = await oauth.auth0.authorize_access_token(request)
-    request.session['token'] = token
-    return RedirectResponse(url='/', status_code=HTTPStatus.TEMPORARY_REDIRECT)
-
-
-@app.route('/login')
-async def login(request):
-    redirect_uri = request.url_for('auth')
-    return await oauth.auth0.authorize_redirect(request, redirect_uri) # redirects to auth0 login page
-
-
-@app.route("/logout")
-async def logout(request: Request):
-    request.session.clear()
-    return RedirectResponse(
-        url=f"https://{config.AUTH0_DOMAIN}/v2/logout?" 
-        + urlencode(
-            {
-                "returnTo": request.url_for('homepage'),
-                "client_id": config.AUTH0_CLIENT_ID,
-            },
-            quote_via=quote_plus,
-        )
-    )
+        return RedirectResponse(url='/login', status_code=HTTPStatus.SEE_OTHER)
     
+app.include_router(router=auth_router)
 
 if __name__ == '__main__':
     uvicorn.run(
