1a4aeaff6939a31449366bfc1d3b4eee4082fb09
Sevelantis <hamer223@o2.pl>
2023-03-16 16:58:50 +0000

    Parametrized certs for HTTPS

--- src/config.py
+++ src/config.py
@@ -17,13 +17,17 @@ AUTH0_ALGORITHMS=env.get('AUTH0_ALGORITHMS', 'default')
 AUTH0_API_AUDIENCE=env.get('AUTH0_API_AUDIENCE', 'default')
 
 # APP
-APP_HOST=env.get('APP_HOST', '0.0.0.0')
+APP_HOST=env.get('APP_HOST', 'localhost')
 APP_PORT=env.get('APP_PORT', 8888)
 APP_SECRET_KEY=env.get('APP_SECRET_KEY', '@#$14TAKE_ON_ME!@#!5take_me_up412312')
+APP_COOKIE_NAME=env.get('APP_COOKIE_NAME', "MY_AWESOME_COOKIE")
 
 # CORS
 BACKEND_CORS_ORIGINS = [
-    f"http://localhost:{APP_PORT}",
+    f"http://{APP_HOST}:{APP_PORT}",
     f"http://127.0.0.1:{APP_PORT}",
-    f"http://127.0.0.0:{APP_PORT}",
+    f"http://localhost:{APP_PORT}",
 ]
+
+# SSL
+SSL_KEYFILE_PASSPHRASE=env.get('SSL_KEYFILE_PASSPHRASE', 'nope:)')
--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 '''main.py is a root of the project, which inits the FastAPI app'''
 
-from http import HTTPStatus
-import json
 from urllib.parse import quote_plus, urlencode
-from fastapi import FastAPI, Request, Response, Depends
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 import uvicorn
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import RedirectResponse
 from docs.dependencies import custom_openapi
 from src.auth.router import auth_router
 from src.conversion.router import conversion_router
@@ -23,22 +19,12 @@ app.add_middleware(
     secret_key=config.APP_SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[str(origin) for origin in config.BACKEND_CORS_ORIGINS],
     allow_credentials=True,
+    allow_origins=[str(origin) for origin in config.BACKEND_CORS_ORIGINS],
     allow_methods=["*"],
     allow_headers=["*"],
     )
 
-
-@app.get('/')
-async def homepage(request: Request):
-    token = request.session.get('token')
-    if token:
-        return JSONResponse(content=json.dumps(token), status_code=HTTPStatus.OK)
-    else:
-        return RedirectResponse(url='/api/v1/login', status_code=HTTPStatus.SEE_OTHER)
-  
-    
 app.include_router(router=auth_router)
 app.include_router(router=conversion_router)
 
@@ -49,4 +35,6 @@ if __name__ == '__main__':
         port=int(config.APP_PORT),
         reload=True,
         server_header=False,
+        ssl_certfile="certs/composer_public_key.pem",
+        ssl_keyfile="certs/composer_private_key.pem"
     )
