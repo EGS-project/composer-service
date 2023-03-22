8959f799a8c25dcffe70e8443301fbfdd3a711c0
Sevelantis <hamer223@o2.pl>
2023-03-22 09:20:17 +0000

    added user dependencies

--- src/user/dependencies.py
+++ src/user/dependencies.py
@@ -1,14 +1,17 @@
 '''dependencies.py - Dependency Injection for router.py'''
 
 from src.auth.utils import decoded_value
+from sqlalchemy.orm import Session
 
 from http import HTTPStatus
 from fastapi import Depends, HTTPException
 from src.auth.dependencies import api_key_cookie
-from src.database.dependencies import db_engine
+from src.database.dependencies import database
 import src.user.models as models
+from src.user.repository import UserRepository
+from src.user.factory import UserFactory
+from src.user.manager import UserManager
 
-models.Base.metadata.create_all(bind=db_engine)
 
 def current_user(cookie: str = Depends(api_key_cookie)):
     try:
@@ -18,3 +21,16 @@ def current_user(cookie: str = Depends(api_key_cookie)):
             status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
             detail="Unprocessable entity.",
         ) from e
+
+def user_repository(database: Session = Depends(database)) -> UserRepository:
+    return UserRepository(db=database)
+
+def user_factory(user_repository: UserRepository = Depends(user_repository)) -> UserFactory:
+    return UserFactory(user_repository=user_repository)
+
+def user_manager(
+    user_factory: UserFactory = Depends(user_factory),
+    user_repository: UserRepository = Depends(user_repository)
+    ) -> UserManager:
+    
+    return UserManager(user_factory=user_factory, user_repository=user_repository)
