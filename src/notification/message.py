e69cd0c5b08047b31aca00e4a5810e191ebe18b4
Sevelantis <hamer223@o2.pl>
2023-04-05 22:16:45 +0100

    added message

---
+++ src/notification/message.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+import base64
+from PIL import Image
+
+
+class NotificationMsg():
+    def __init__(self) -> None:
+        pass
+    
+    def serialize(self):
+        return 'todo'
