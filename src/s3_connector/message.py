8fdda3d8fad3fefb7c16cbf2ec763b3abd5c33f6
Sevelantis <hamer223@o2.pl>
2023-04-07 01:47:01 +0100

    Added base messages serialization/deserialization for s3 connec

--- src/s3_connector/message.py
+++ src/s3_connector/message.py
@@ -1,31 +1,51 @@
-import xml.etree.ElementTree as ET
-import base64
-from PIL import Image
+import email
+from email.mime.image import MIMEImage
+from email.mime.message import MIMEMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import stomp
+import stomp.utils
 from src.activemq.cache.message import CachedMessage
 
-
 class StoreImageMsg(CachedMessage):
-    def __init__(self, correlation_id: str, filename: str, image_data: bytes) -> None:
+    def __init__(
+        self, 
+        correlation_id: str = None,
+        filename: str = None, 
+        image_data: bytes = None
+        ) -> None:
         CachedMessage.__init__(self, correlation_id=correlation_id)
         self.filename = filename
         self.image_data = image_data
             
     def serialize(self):
-        xml_root = ET.Element("store_image")
-        xml_filename = ET.SubElement(xml_root, "filename")
-        xml_filename.text = self.filename
-        # todo serialize MIME
-        return ET.tostring(xml_root, encoding='utf-8', xml_declaration=True)
+        pass
+    
+    def deserialize(self, frame: stomp.utils.Frame):
+        mime_message: MIMEMessage = email.message_from_string(frame.body)
+        for part in mime_message.walk():
+            if part.get('Content-ID') == 'image_data':
+                self.image_data = part.get_payload(decode=True)
+            elif part.get('Content-ID') == 'filename':
+                self.image_format = part.get_payload(decode=True)
+        self.correlation_id = frame.headers.get('correlation_id')
     
 class StoreImageReplyMsg(CachedMessage):
-    def __init__(self, correlation_id: str) -> None:
+    def __init__(
+        self,
+        correlation_id: str
+        ) -> None:
         CachedMessage.__init__(self, correlation_id=correlation_id)
-            
-    def deserialize(self, message):
-        xml_root = ET.fromstring(message.body)
-        xml_root = xml_root.find("get_image_reply").text
-        self.url= xml_root.find("url").text
-        self.correlation_id = message.headers['correlation_id']
+    
+    def serialize(self):
+        mime_multipart = MIMEMultipart()
+        part = MIMEText(self.url)
+        part.add_header('Content-ID', 'url')
+        mime_multipart.attach(part)
+        return mime_multipart.as_string()  
+    
+    def deserialize(self, frame: stomp.utils.Frame):
+        pass
 
 class GetImageMsg(CachedMessage):
     def __init__(self, correlation_id: str, filename: str) -> None:
@@ -33,28 +53,25 @@ class GetImageMsg(CachedMessage):
         self.filename = filename
             
     def serialize(self):
-        xml_root = ET.Element("get_image")
-        xml_filename = ET.SubElement(xml_root, "filename")
-        xml_filename.text = self.filename
-        return ET.tostring(xml_root, encoding='utf-8', xml_declaration=True)
+        pass        
+    
+    def deserialize(self, frame: stomp.utils.Frame):
+        pass
     
 class GetImageReplyMsg(CachedMessage):
     def __init__(self, correlation_id: str, url: str) -> None:
         CachedMessage.__init__(self, correlation_id=correlation_id)
         self.url = url
             
-    def deserialize(self, message):
-        xml_root = ET.fromstring(message.body)
-        xml_root = xml_root.find("get_image_reply").text
-        self.url= xml_root.find("url").text
-        self.correlation_id = message.headers['correlation_id']
+    def serialize(self):
+        pass        
+    
+    def deserialize(self, frame: stomp.utils.Frame):
+        pass
 
 class DeleteImageMsg():
     def __init__(self, filename: str) -> None:
         self.filename = filename
             
     def serialize(self):
-        xml_root = ET.Element("delete_image")
-        xml_filename = ET.SubElement(xml_root, "filename")
-        xml_filename.text = self.filename
-        return ET.tostring(xml_root, encoding='utf-8', xml_declaration=True)
+        pass        
