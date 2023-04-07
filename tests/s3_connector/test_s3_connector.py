9b36094bc22cd7862df27fcab8febd6326485711
Sevelantis <hamer223@o2.pl>
2023-04-07 01:47:13 +0100

    added base tests for s3 connect

--- tests/s3_connector/test_s3_connector.py
+++ tests/s3_connector/test_s3_connector.py
@@ -1,5 +1,7 @@
 import pytest
 import stomp
+import stomp.utils
+from src.activemq.manager import ActivemqWorkerManager
 from src.s3_connector.message import GetImageMsg, StoreImageMsg
 from src.activemq.dispatcher import ActivemqDispatcher
 from src.activemq.factory import ActiveMqConnectionFactory, MessageFactory
@@ -13,8 +15,8 @@ def store_image_conn() -> stomp.connect.StompConnection11:
     class MockedS3ConnectorStoreImageListener(stomp.ConnectionListener):
         def __init__(self) -> None:
             super().__init__()
-        def on_message(self, message):
-            logging.info(f'new store image message: {message}')
+        def on_message(self, frame: stomp.utils.Frame):
+            msg = StoreImageMsg()
     
     return ActiveMqConnectionFactory.create_connection(
         broker_host=config.ACTIVEMQ_HOST,
@@ -72,13 +74,27 @@ def mock_store_image_msg(message_factory: MessageFactory) -> StoreImageMsg:
 def mock_get_image_msg(message_factory: MessageFactory) -> GetImageMsg:
     return message_factory.create_get_image_message()
 
-def test_get_image(
+def test_store_image(
     activemq_dispatcher: ActivemqDispatcher, 
-    get_image_reply_conn: stomp.connect.StompConnection11,
-    mock_store_image_msg: StoreImageMsg
+    activemq_worker_manager: ActivemqWorkerManager,
+    mocked_convert_image_msg: ConvertImageMsg,
+    mocked_convert_image_reply_msg: ConvertImageReplyMsg,
+    activemq_cache_manager: ActivemqCacheManager
     ):
     assert type(activemq_dispatcher) == ActivemqDispatcher
-    assert type(get_image_reply_conn) == stomp.connect.StompConnection11
-    assert type(mock_store_image_msg) == StoreImageMsg
-    # activemq_dispatcher.send_store_image_message(msg=mock_store_image_msg)
+    assert type(activemq_worker_manager) == ActivemqWorkerManager
+    assert type(mocked_convert_image_msg) == ConvertImageMsg
+    assert type(mocked_convert_image_reply_msg) == ConvertImageReplyMsg
+    assert type(activemq_cache_manager) == ActivemqCacheManager
     
+    activemq_worker_manager.submit_threadpool()
+
+    activemq_dispatcher.send_convert_image_message(
+        msg=mocked_convert_image_msg
+        )
+    
+    assert activemq_cache_manager.await_reply_message(
+        correlation_id=mocked_convert_image_msg.correlation_id
+        ) == mocked_convert_image_reply_msg
+    
+    activemq_worker_manager.stop_threadpool()
