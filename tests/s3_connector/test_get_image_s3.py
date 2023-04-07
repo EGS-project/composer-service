99add4990e4723619789e573fda0705ae873e4d6
Sevelantis <hamer223@o2.pl>
2023-04-07 17:04:01 +0100

    Passing get image contracts' tests.

---
+++ tests/s3_connector/test_get_image_s3.py
@@ -0,0 +1,99 @@
+import pytest
+import stomp
+import stomp.utils
+from src.activemq.utils import SubIdGenerator
+from src.activemq.worker import ActiveMqWorker
+from src.activemq.manager import ActivemqWorkerManager
+from src.s3_connector.message import GetImageMsg, GetImageReplyMsg, StoreImageMsg, StoreImageReplyMsg
+from src.activemq.dispatcher import ActivemqDispatcher
+from src.activemq.factory import ActiveMqConnectionFactory, ActivemqWorkerFactory, MessageFactory
+from src.s3_connector.listener import GetImageReplyListener, StoreImageReplyListener
+import src.config as config
+import logging
+from tests.test_base import *
+
+@pytest.fixture()
+def mocked_get_image_worker(
+    send_conn: stomp.connect.StompConnection11,
+    mocked_get_image_reply_msg: GetImageReplyMsg
+    ) -> ActiveMqWorker:
+    class MockedGetImageListener(stomp.ConnectionListener):
+        def __init__(self) -> None:
+            super().__init__()
+        def on_message(self, frame: stomp.utils.Frame):
+            msg = GetImageMsg()
+            msg.deserialize(frame=frame)
+            send_conn.send(
+                destination=config.ACTIVEMQ_GET_IMAGE_REPLY_QUEUE,
+                body=mocked_get_image_reply_msg.serialize(),
+                headers={
+                    'content-type': 'multipart/mixed',
+                    'correlation_id': mocked_get_image_reply_msg.correlation_id
+                    }
+            )
+            
+    return ActiveMqWorker(
+        connection=ActiveMqConnectionFactory.create_connection(
+            listener=MockedGetImageListener()
+        ),
+        sub_id=SubIdGenerator.generate_next(),
+        queue=config.ACTIVEMQ_GET_IMAGE_QUEUE
+    )
+    
+@pytest.fixture()
+def get_image_reply_worker():
+    return ActivemqWorkerFactory().create_get_image_reply_worker()
+
+
+@pytest.fixture()
+def activemq_worker_manager(
+    mocked_get_image_worker: ActiveMqWorker,
+    get_image_reply_worker: ActiveMqWorker,
+):
+    return ActivemqWorkerManager(workers=[
+            mocked_get_image_worker,
+            get_image_reply_worker
+    ])
+    
+@pytest.fixture()
+def mocked_get_image_msg(
+    message_factory: MessageFactory
+    ) -> StoreImageMsg:
+    return message_factory.create_get_image_message(
+        filename='raccoon.png',
+        correlation_id='generate_me_pls_456'
+    )
+    
+    
+@pytest.fixture()
+def mocked_get_image_reply_msg() -> GetImageReplyMsg:
+    return GetImageReplyMsg(
+        url='www.thisismydownloadsuperurl.pls/raccoon.png',
+        correlation_id='generate_me_pls_456'
+        )
+
+
+def test_get_image(
+    activemq_dispatcher: ActivemqDispatcher, 
+    activemq_worker_manager: ActivemqWorkerManager,
+    mocked_get_image_msg: GetImageMsg,
+    mocked_get_image_reply_msg: GetImageReplyMsg,
+    activemq_cache_manager: ActivemqCacheManager
+    ):
+    assert type(activemq_dispatcher) == ActivemqDispatcher
+    assert type(activemq_worker_manager) == ActivemqWorkerManager
+    assert type(mocked_get_image_msg) == GetImageMsg
+    assert type(mocked_get_image_reply_msg) == GetImageReplyMsg
+    assert type(activemq_cache_manager) == ActivemqCacheManager
+    
+    activemq_worker_manager.submit_threadpool()
+
+    activemq_dispatcher.send_get_image_message(
+        msg=mocked_get_image_msg
+        )
+    
+    assert activemq_cache_manager.await_reply_message(
+        correlation_id=mocked_get_image_msg.correlation_id
+        ) == mocked_get_image_reply_msg
+    
+    activemq_worker_manager.stop_threadpool()
