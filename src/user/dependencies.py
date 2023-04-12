2cac9d2d3f282375447478b3f1d14e1e88b8ae87
Sevelantis <hamer223@o2.pl>
2023-04-12 10:01:16 +0100

    Added current user DI

--- src/user/dependencies.py
+++ src/user/dependencies.py
@@ -1,17 +1,17 @@
 '''dependencies.py - Dependency Injection for router.py'''
 
-import json
-from src.auth.schemas import SessionData
-from src.auth.utils import decoded_value
+from http import HTTPStatus
+
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from http import HTTPStatus
-from fastapi import Depends, HTTPException
-from src.auth.dependencies import api_key_cookie
+import src.user.models as models
+from src.auth.dependencies import session_data
+from src.auth.schemas import SessionData
 from src.database.dependencies import database
-from src.user.repository import UserRepository
 from src.user.factory import UserFactory
 from src.user.manager import UserManager
+from src.user.repository import UserRepository
 
 
 def user_repository(database: Session = Depends(database)) -> UserRepository:
@@ -26,3 +26,15 @@ def user_manager(
     ) -> UserManager:
     
     return UserManager(user_factory=user_factory, user_repository=user_repository)
+
+def current_user(
+    session_data: SessionData = Depends(session_data),
+    user_repository: UserRepository = Depends(user_repository)
+    ) -> models.User:
+    try:
+        return user_repository.get_user(user_id=session_data.user_id)
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail="Unprocessable entity.",
+        ) from e
